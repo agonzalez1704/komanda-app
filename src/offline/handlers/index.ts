6e6b7e0af6b52d93b7862a5f6e48136806580170
antonio-gonzalez-svg <agonzalez.nrn02@gmail.com>
2026-04-22 08:53:00 -0600

    feat(offline): useQueueDrain hook wired at root layout

--- src/offline/handlers/index.ts
+++ src/offline/handlers/index.ts
@@ -1,5 +1,6 @@
 import type { HandlerRegistry } from '@/offline/processor';
 import { createLocalStore } from '@/offline/localStore';
+import { createQueueStore } from '@/offline/queue';
 import { createKomandaHandler } from './createKomanda';
 import { renameKomandaHandler } from './renameKomanda';
 import { updateStatusHandler } from './updateStatus';
@@ -8,6 +9,7 @@ import { updateItemHandler } from './updateItem';
 import { removeItemHandler } from './removeItem';
 import { closeKomandaHandler } from './closeKomanda';
 
+export const queueStore = createQueueStore();
 export const localStore = createLocalStore();
 
 export const handlers: HandlerRegistry = {
